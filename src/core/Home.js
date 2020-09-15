9874befccee1d73be03669459f060bd8baf7471c
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-09-14 19:08:16 -0500

    creating Signup page

---
+++ src/core/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home =()=> (
+    <div className="jumbotron">
+    
+    <h2> Home</h2>
+    <p>Welcome to Social Media Client!</p>
+    
+    </div>
+
+
+);
+
+export default Home;
---
+++ src/user/Signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Signup</h2>
+      </div>
+    );
+  }
+}
+export default Signup;
