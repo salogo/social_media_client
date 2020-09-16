ec1c6dc6491e24ce6cc62ef0196995895241b255
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-09-16 15:13:29 -0500

    creating Sign Out page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,9 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin"
+import Menu from "./core/Menu";
 
 const MainRouter = () => (
     <div>
+    <Menu />
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={Signup} /> 
